e34cc4dada7bcc7c54d5bc9a89db82458a47fc98
Mike-Yates <70710481+Mike-Yates@users.noreply.github.com>
2022-03-23 22:25:09 -0400

    database is connected, front end of friends form is working and input checking.

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,7 @@ import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 import 'package:mysql1/mysql1.dart';
 
-Future main() async {
-  final conn = await MySqlConnection.connect(ConnectionSettings(
-      host: 'mysql01.cs.virginia.edu',
-      port: 3306,
-      user: 'mjy5xy', // root didnt work
-      db: 'mjy5xy',
-      password: 'Winter2022!!'));
-
-  await conn.close();
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -53,17 +44,48 @@ class _MyCustomFormState extends State<MyCustomForm> {
     super.dispose();
   }
 
-  /*
-  bool add_row_to_friends(name, major, year) {
-    var result = await conn.query(
-      'insert into friends (name, major, year) values (?, ?, ?)',
-      [myController.text, myController2.text, myController3.text]);
-      print('Inserted row id=${result.insertId}');
-
-
-    return true; 
+  bool checkInput(name, major, year) {
+    if (name == '' || major == '' || year == '') {
+      return false;
+    }
+    if (name == null || major == null || year == null) {
+      return false;
+    }
+    try {
+      int.parse(year);
+    } catch (e) {
+      return false;
+    }
+    int x = int.parse(year);
+    if (x > 4 || x < 0) {
+      return false;
+    }
+    return true;
   }
-  */
+
+  Future<bool> add_row_to_friends(name, major, year) async {
+    final conn = await MySqlConnection.connect(ConnectionSettings(
+        host: 'mysql01.cs.virginia.edu',
+        port: 3306,
+        user: 'mjy5xy', // root didnt work
+        db: 'mjy5xy',
+        password:
+            'Winter2022!!')); // in the future, password of database should not be used. how do i do this?
+    try {
+      var result = await conn.query(
+          'insert into friends (name, major, year) values (?, ?, ?)',
+          [name, major, year]);
+      print('Inserted row id=${result.insertId}');
+      myController.clear();
+      myController2.clear();
+      myController3.clear();
+      return true;
+    } catch (e) {
+      print(e);
+      print("error was in add_row_to_friends.");
+      return false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -164,7 +186,24 @@ class _MyCustomFormState extends State<MyCustomForm> {
                       style: TextButton.styleFrom(
                         primary: Colors.blue,
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        bool input_correctness = checkInput(myController.text,
+                            myController2.text, myController3.text);
+                        if (!input_correctness) {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                // call on password calculator function
+                                content: Text("Incorrect Input. Please Fix. "),
+                              );
+                            },
+                          );
+                        } else {
+                          add_row_to_friends(myController.text,
+                              myController2.text, myController3.text);
+                        }
+                      },
                       child: Text('Add', style: TextStyle(color: Colors.white)),
                     ),
                     SizedBox(width: 25.0),
