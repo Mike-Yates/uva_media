e0cd6339bc302dee441801d026f79dfe505dd621
justinlogan03 <jhl3mn@virginia.edu>
2022-04-14 19:33:05 -0400

    Update feed.dart

--- lib/Screens/feed.dart
+++ lib/Screens/feed.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mysql1/mysql1.dart';
+
 class HomeView extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("UVA Media"),
@@ -12,23 +13,23 @@ class HomeView extends StatelessWidget {
   }
 
 
-  Widget _postText(user_text, post_text){
+  Widget _postText(user_text, post_text) {
     return Padding(
       padding: const EdgeInsets.symmetric(
         horizontal: 8,
         vertical: 4,
       ),
       child: ListTile(
-        title: Text(user_text,
-          style: TextStyle(fontWeight: FontWeight.bold
-        ),
-      ),
-        subtitle: Text(post_text)
+          title: Text(user_text,
+            style: TextStyle(fontWeight: FontWeight.bold
+            ),
+          ),
+          subtitle: Text(post_text)
       ),
     );
   }
 
-  Widget _postCommentButton(){
+  Widget _postCommentButton() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 45),
       child: GestureDetector(
@@ -41,7 +42,7 @@ class HomeView extends StatelessWidget {
     );
   }
 
-  Widget _postView(user_text, post_text){
+  Widget _postView(user_text, post_text) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [ _postText(user_text, post_text), _postCommentButton()],
@@ -58,11 +59,15 @@ class HomeView extends StatelessWidget {
         'Winter2022!!')); // in the future, password of database should not be used. how do i do this?
     try {
       var result = await conn
-          .query('select * from Active_Users where email = ?', [user.text]);
+          .query('select post_text & post_id from Posts', [text, id]);
+      }catch (e) {
+      print(e);
+      // return false;
+    }
     return ListView.builder(
-      itemCount: 8,
-      itemBuilder: (context, index){
-        return _postView("username", "Look at all these posts!!! So cool!!! I love UVA media so much! More text to see how it looks? Yes please!");
-      });
+        itemCount: 8,
+        itemBuilder: (context, index) {
+          return _postView("username",
+              "Look at all these posts!!! So cool!!! I love UVA media so much! More text to see how it looks? Yes please!");
+        });
   }
-}
