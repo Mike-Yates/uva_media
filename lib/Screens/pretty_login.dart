0dcf360103f2d2adc93d1f2ee8f021c7257991a0
Mike-Yates <70710481+Mike-Yates@users.noreply.github.com>
2022-04-14 19:07:53 -0400

    pretty login page code added

---
+++ lib/Screens/pretty_login.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:uva_media/screens/home.dart';
+import 'package:uva_media/screens/make_post.dart';
+import 'package:mysql1/mysql1.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+
+class LoginScreen2 extends StatefulWidget {
+  const LoginScreen2({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreen2 createState() => _LoginScreen2();
+}
+
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _LoginScreen2 extends State<LoginScreen2> {
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  Future<String?> _authUser(LoginData data) async {
+    //added async
+    debugPrint('Name: ${data.name}, Password: ${data.password}');
+
+    final conn = await MySqlConnection.connect(ConnectionSettings(
+        host: 'mysql01.cs.virginia.edu',
+        port: 3306,
+        user: 'mjy5xy',
+        db: 'mjy5xy',
+        password:
+            'Winter2022!!')); // in the future, password of database should not be used. how do i do this?
+    try {
+      var result = await conn
+          .query('select * from Active_Users where email = ?', [data.name]);
+      if ((result.isEmpty)) {
+        return Future.delayed(loginTime).then((_) {
+          return 'User not exists';
+        });
+      } else {
+        return Future.delayed(loginTime).then((_) {
+          return null;
+        });
+      }
+      // return true;
+    } catch (e) {
+      print(e);
+      print("error was in add_row_to_friends.");
+      // return false;
+    }
+  }
+
+  Future<String?> _signupUser(SignupData data) async {
+    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
+    final conn = await MySqlConnection.connect(ConnectionSettings(
+        host: 'mysql01.cs.virginia.edu',
+        port: 3306,
+        user: 'mjy5xy',
+        db: 'mjy5xy',
+        password:
+            'Winter2022!!')); // in the future, password of database should not be used. how do i do this?
+    try {
+      var result = await conn.query(
+          'insert into Active_Users (email, password, points, reports) values (?, ?, ?, ?)',
+          [data.name, data.password, 0, 0]);
+      print('Inserted row id=${result.insertId}');
+      return Future.delayed(loginTime).then((_) {
+        return null;
+      });
+    } catch (e) {
+      print(e);
+      print("error was in add_row_to_friends.");
+      // return false;
+      return Future.delayed(loginTime).then((_) {
+        return null;
+      });
+    }
+  }
+
+  Future<String> _recoverPassword(String name) async {
+    // this hasnt been tested yet
+    debugPrint('Name: $name');
+    final conn = await MySqlConnection.connect(ConnectionSettings(
+        host: 'mysql01.cs.virginia.edu',
+        port: 3306,
+        user: 'mjy5xy',
+        db: 'mjy5xy',
+        password:
+            'Winter2022!!')); // in the future, password of database should not be used. how do i do this?
+    try {
+      var result = await conn
+          .query('select * from Active_Users where email = ?', [name]);
+      if ((result.isEmpty)) {
+        return Future.delayed(loginTime).then((_) {
+          return 'User not exists';
+        });
+      } else {
+        return 'null';
+      }
+      // return true;
+    } catch (e) {
+      print(e);
+      return 'error in _recoverPassword, check login.dart';
+      // return false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      title: 'UVA Media',
+      logo: const AssetImage('assets/TPP.PNG'),
+      onLogin: _authUser,
+      onSignup: _signupUser,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) =>
+              MyCustomForm(), // this decides where user is taken after logging in  --------------------------------------------------
+        ));
+      },
+      onRecoverPassword: _recoverPassword,
+    );
+  }
+}
