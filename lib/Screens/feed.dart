58d4d9348307fa9609a26c70a95408ebfc82d83f
justinlogan03 <jhl3mn@virginia.edu>
2022-04-14 20:24:53 -0400

    Update feed.dart

--- lib/Screens/feed.dart
+++ lib/Screens/feed.dart
@@ -49,7 +49,7 @@ class HomeView extends StatelessWidget {
     );
   }
 
-  Future<Widget> _postListView() async {
+  Future<Object> _postPull() async {
     final conn = await MySqlConnection.connect(ConnectionSettings(
         host: 'mysql01.cs.virginia.edu',
         port: 3306,
@@ -59,15 +59,42 @@ class HomeView extends StatelessWidget {
         'Winter2022!!')); // in the future, password of database should not be used. how do i do this?
     try {
       var result = await conn
-          .query('select post_text & post_id from Posts', [text, id]);
-      }catch (e) {
+          .query('select post_id, post_text from Post');
+      return result;
+    } catch (e) {
       print(e);
+      return e;
       // return false;
     }
+  }
+
+  Future<Object> _postNum() async {
+    final conn = await MySqlConnection.connect(ConnectionSettings(
+        host: 'mysql01.cs.virginia.edu',
+        port: 3306,
+        user: 'mjy5xy',
+        db: 'mjy5xy',
+        password:
+        'Winter2022!!')); // in the future, password of database should not be used. how do i do this?
+    try {
+      var count = await conn
+          .query('SELECT COUNT(*) FROM Post');
+      return count;
+    } catch (e) {
+      print(e);
+      return e;
+      // return false;
+    }
+  }
+
+  Widget _postListView() {
+    var result = _postPull();
+    var num = _postNum().toString();
     return ListView.builder(
-        itemCount: 8,
+        itemCount: 3,
         itemBuilder: (context, index) {
-          return _postView("username",
+          return _postView(num,
               "Look at all these posts!!! So cool!!! I love UVA media so much! More text to see how it looks? Yes please!");
         });
   }
+}
